0e7ea596310c269b94394e2a1276c33344d31525
devaanantawijaya <gededeva11@gmail.com>
2025-02-14 14:08:26 +0800

    ganti jadi link page

--- src/hooks/useUsers.jsx
+++ src/hooks/useUsers.jsx
@@ -4,17 +4,23 @@ import { BASE_URL } from "../helper/endpoint";
 
 const useAllUsers = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const getAllUsers = async () => {
+  const getAllUsers = async (page = 1) => {
     try {
-      const res = await axios.get(`${BASE_URL.API}/?results=10`);
+      setLoading(true);
+      const res = await axios.get(
+        `${BASE_URL.API}/?page=${page}&results=10&seed=abc`
+      );
       setUsers(res.data.results);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
-  return { users, getAllUsers };
+  return { users, getAllUsers, loading };
 };
 
 export default useAllUsers;
